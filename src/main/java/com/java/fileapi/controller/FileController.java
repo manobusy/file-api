3df7628818bbe79ed4df79b6ff56bcc665d4590e
manobusy <manobusy@gmail.com>
2018-05-12 07:40:43 -0500

    Added File Operation landing page

--- src/main/java/com/java/fileapi/controller/FileController.java
+++ src/main/java/com/java/fileapi/controller/FileController.java
@@ -1,5 +1,14 @@
 package com.java.fileapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/fileapi/v1")
 public class FileController {
 
+	@RequestMapping("/landing")
+	public String landing(){
+		return "Welcome to File Operation API Page";
+	}
 }
